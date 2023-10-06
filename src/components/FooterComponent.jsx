53c0d47210e484c3e4eb85564dacd192098fc950
AminGangsarPangarso <amindailylife1976@gmail.com>
2023-10-06 11:32:04 +0700

    selesai footer dan semua kelas

--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -1,22 +1,54 @@
 import { Container, Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const FooterComponent = () => {
     return (
-        <div className="footer">
+        <div className="footer py-5">
             <Container>
-                <Row>
-                    <Col className="sosial">
+                <Row className="d-flex justify-content-between">
+                    <Col lg="5" className="" >
                         <div className="sosial-footer ">
-                            <h2 className="fw-bold">NGODING</h2>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, rem illo quos aliquid perspiciatis dignissimos pariatur quibusdam neque laudantium quas.</p>
-                            <div className="contact">
-                            <a href=""><i className="fa-brands fa-whatsapp">+62 081804828974</i></a>
-                            <a href=""><i className="fa-regular fa-envelope">amindailylife1976@gmail.com</i></a>
+                            <h3 className="fw-bold">NGODING</h3>
+                            <p className="desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, rem illo quos aliquid perspiciatis dignissimos pariatur quibusdam neque laudantium quas.</p>
+                            <div className="no mb-1 mt-4">
+                                <Link className="text-decoration-none"><i className="fa-brands fa-whatsapp"></i>
+                                    <p className="m-0">+62 81804838974</p>
+                                </Link>
+                                <div className="email">
+                                    <Link className="text-decoration-none"><i className="fa-regular fa-envelope"></i>
+                                        <p className="m-0">amindailylife1976@gmail.com</p>
+                                    </Link>
+                                </div>
                             </div>
                         </div>
                     </Col>
-                    <Col>2 of 3</Col>
-                    <Col>3 of 3</Col>
+                    <Col className="d-flex flex-column col-lg-2 mt-lg-0 mt-5">
+                        <h5 className="fw-bold">Menu</h5>
+                        <Link to="/" >Home</Link>
+                        <Link to="kelas" >Kelas</Link>
+                        <Link to="testimonial">Testimonial</Link>
+                        <Link to="faq"> FAQ</Link>
+                        <Link to="syaratketen">Syarat & Ketentuan</Link>
+                    </Col>
+                    <Col lg="4" className="mt-lg-0 mt-5">
+                        <h5 className="fw-bold mb-1">Subscribe untuk info Menarik</h5>
+                        <div className="subscribe">
+                            <input type="text" name="" id="" placeholder="subcribe..." />
+                            <button className="rounded-end rounded-0">Subscribe</button>
+                        </div>
+                        <div className="social mt-3">
+                            <i className="fa-brands fa-facebook"></i>
+                            <i className="fa-brands fa-twitter"></i>
+                            <i className="fa-brands fa-linkedin"></i>
+                            <i className="fa-brands fa-youtube"></i>
+
+                        </div>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                    <p className="text-center px-md-0 px-3">© Copyright {new Date().getFullYear()} by <span className="fw-bold"> Amin Gangsar Pangarso</span> , All Right Reserved</p>
+                    </Col>
                 </Row>
             </Container>
         </div>
--- src/pages/KelasPage.jsx
+++ src/pages/KelasPage.jsx
@@ -1,7 +1,43 @@
+import { Container, Row, Col } from "react-bootstrap";
+import { semuaKelas } from '../data/index'
 const KelasPage = () => {
-    return ( 
-        <h1>ini bagian kelas</h1>
-     );
+    return (
+        <div className="kelas-page">
+            <div className="kelas min-vh-100">
+                <Container>
+                    <Row>
+                        <Col>
+                            <h1 className="fw-bold text-center" >Semua Kelas</h1>
+                            <p className="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi explicabo soluta nobis</p>
+
+                        </Col>
+                    </Row>
+                    <Row>
+                    {semuaKelas.map((kelas) => {
+                            return (
+
+                                <Col key={kelas.id} className="shadow rounded-1">
+                                    <img src={kelas.image} alt="unsplash.com" className="mb-5 rounded-top" />
+                                    <div className="star mb-2">
+                                        <i className={kelas.star1}></i>
+                                        <i className={kelas.star2}></i>
+                                        <i className={kelas.star3}></i>
+                                        <i className={kelas.star4}></i>
+                                        <i className={kelas.star5}></i>
+                                    </div>
+                                    <h5>{kelas.title}</h5>
+                                    <div className="ket d-flex justify-content-between aling-items-center px-3 pb-3">
+                                        <p className="m-0 text-primary fw-bold">{kelas.price}</p>
+                                        <button className="btn btn-danger rounded-1">{kelas.buy}</button>
+                                    </div>
+                                </Col>
+                            )
+                        })}
+                    </Row>
+                </Container>
+            </div>
+        </div>
+    );
 }
- 
+
 export default KelasPage;
